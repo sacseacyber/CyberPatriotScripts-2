cd3b6cf74409095066d0da509b2926f97b40b44c
William <wpc150@gmail.com>
2019-01-05 20:19:09 -0500

    Update First.bat

--- Windows/First.bat
+++ Windows/First.bat
@@ -1,1 +1,10 @@
-#stuff
+@echo off
+echo Checking if script has Administrative permissions...
+net sessions
+if %errorlevel%==0 (
+echo Success!
+) else (
+echo No admin, please run with Administrative rights...
+pause
+exit
+)
